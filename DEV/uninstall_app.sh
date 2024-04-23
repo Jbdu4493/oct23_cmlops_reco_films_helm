f749d5ff91c875279cbcf5c722450ceac038e677
“ssasnouski” <sosnovski.sergey@gmail.com>
2024-04-23 18:59:40 +0200

    Modify uninstall_app.sh

--- DEV/uninstall_app.sh
+++ DEV/uninstall_app.sh
@@ -5,8 +5,12 @@ kubectl delete -f https://raw.githubusercontent.com/Jbdu4493/oct23_cmlops_reco_f
 kubectl delete -f https://raw.githubusercontent.com/Jbdu4493/oct23_cmlops_reco_films_helm/main/DEV/MLFLOW/application_argocd.yaml
 kubectl delete -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
 
+kubectl delete all --all -n argocd
+kubectl delete all --all -n app-movieflix-dev
+
+kubectl delete statefulset airflow-postgresql -n default
+kubectl delete crds --selector app.kubernetes.io/part-of=argocd
+kubectl delete clusterrole,clusterrolebinding --selector app.kubernetes.io/part-of=argocd
+
 kubectl delete namespace argocd
 kubectl delete namespace app-movieflix-dev
-
-kubectl delete statefulset airflow-postgresql
-
